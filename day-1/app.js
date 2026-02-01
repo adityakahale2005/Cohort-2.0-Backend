3a2030171b96544b4b949da4a6673e6a6ed02092
adityakahale2005 <adikahale@gmail.com>
2026-02-01 19:27:19 +0530

    day-1: node basics

---
+++ day-1/app.js
@@ -0,0 +1,9 @@
+// day-1 revision marker
+// final day-1 revision
+
+
+const express = require("express")
+
+const app = express() // server create kr chuke hai
+
+app.listen(3000) // server ko start krta hai

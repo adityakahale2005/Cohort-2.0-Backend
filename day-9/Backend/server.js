03b1182ac08990a1cfaef1f879924637860fec03
adityakahale2005 <adikahale@gmail.com>
2026-02-02 22:46:27 +0530

    feat: initialize backend with Express and MongoDB integration
    - created package.json with necessary dependencies
    - implemented server setup in server.js
    - added app.js for handling API routes for notes
    - configured MongoDB connection in database.js
    - defined note model in note.model.js

---
+++ day-9/Backend/server.js
@@ -0,0 +1,13 @@
+// work - to start the server , to conect db
+
+require('dotenv').config()
+const app = require("./src/app")
+const connectToDb = require("./src/config/database")
+
+
+connectToDb()
+
+
+app.listen(3000,() => {
+    console.log("server is running");
+})
